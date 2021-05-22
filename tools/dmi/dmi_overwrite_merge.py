82f38ee20a6dbd086239f41def78dda9f3033dd9
silicons <2003111+silicons@users.noreply.github.com>
2021-05-21 23:33:04 -0700

    3

--- tools/dmi/dmi_overwrite_merge.py
+++ tools/dmi/dmi_overwrite_merge.py
@@ -1,6 +1,7 @@
 import dmi
 import os
 import sys
+from dmi import merge_driver
 
 # Merge one DMI into the other
 # Overwrites all conflicting states
@@ -9,12 +10,12 @@ def dmi_overwrite_merge(frompath, topath):
     # grab both sets of dmis
     try:
         src = dmi.Dmi.from_file(frompath)
-    except:
+    except Exception:
         print("Skipping " + frompath + " --> " + topath + ": Could not parse origin.")
         return
     try:
         dest = dmi.Dmi.from_file(topath)
-    except:
+    except Exception:
         print("Skipping " + frompath + " --> " + topath + ": Could not parse destination.")
         return
     width = src.width
@@ -26,13 +27,32 @@ def dmi_overwrite_merge(frompath, topath):
         print("Skipping " + frompath + " --> " + topath + ": Heights don't match.")
         return
     final = dmi.Dmi(width, height)
+    changed = False
+    overwrote = 0
+    appended = 0
     built = dest.states.copy()
     for state in src.states:
         name = state.name
-        for other in built:
-            if other.name == name:
-                built.remove(other)
-        built.append(state)
+        movement = state.movement
+        found = False
+        for index in range(0, len(built)):
+            other = built[index]
+            if (other.name != name) or (other.movement != movement):
+                continue
+            if not merge_driver.states_equal(state, other):
+                built[index] = state
+                changed = True
+                overwrote += 1
+            found = True
+            break
+        if not found:
+            built.append(state)
+            changed = True
+            appended += 1
+    if not changed:
+        print("No change: " + frompath + " == " + topath)
+        return
+    print("Writing " + frompath + " --> " + topath + " with " + str(overwrote) + " states overwritten and " + str(appended) + " states appended.")
     final.states = built
     os.remove(topath)
     final.to_file(topath)
