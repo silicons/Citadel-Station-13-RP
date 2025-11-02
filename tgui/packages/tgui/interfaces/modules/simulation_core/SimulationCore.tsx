0790be6e49cbebc9a6c3589b182a9be100da52de
silicons <2003111+silicons@users.noreply.github.com>
2025-11-01 19:52:53 -0700

    sigh

---
+++ tgui/packages/tgui/interfaces/modules/simulation_core/SimulationCore.tsx
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @license MIT
+ */
+
+export interface SimulationCoreData {
+
+}
+
+export const SimulationCore = (props) => {
+
+};
---
+++ tgui/packages/tgui/interfaces/modules/simulation_core/SimulationCoreResident.tsx
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @license MIT
+ */
+
+export interface SimulationCoreResidentData {
+
+}
+
+export const SimulationCoreResident = (props) => {
+
+};
